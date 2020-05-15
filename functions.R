fcb8084da70a109091b53637efaa5ee47ef8c688
mustafapir <mustafapir29@gmail.com>
2020-05-15 18:38:05 +0300

    Fix bugs

--- functions.R
+++ functions.R
@@ -1,13 +1,6 @@
 
 library(cicerone)
 
-download.file(
-  url = 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js',
-  destfile = 'www/js.cookie.js'
-)
-
-addResourcePath("js", "www")
-
 
 guide <- Cicerone$
   new()$ 
@@ -22,23 +15,3 @@ guide <- Cicerone$
     "Instead of searching a gene, here you can explore our gene list with scores."
   )
 
-
-jsCode <- '
-  shinyjs.getcookie = function(params) {
-    var cookie = Cookies.get("id");
-    if (typeof cookie !== "undefined") {
-      Shiny.onInputChange("jscookie", cookie);
-    }
-  }
-  shinyjs.setcookie = function(params) {
-    Cookies.set("id", escape(params), { expires: 0.5 });
-    Shiny.onInputChange("jscookie", params);
-  }
-  shinyjs.rmcookie = function(params) {
-    Cookies.remove("id");
-    Shiny.onInputChange("jscookie", "");
-  }
-  shinyjs.reload = function() {
-    history.go(0);
-  }
-'
