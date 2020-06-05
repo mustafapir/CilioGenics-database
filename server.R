9a45af26e09d945c361552c8eaa845105be4f0a2
mustafapir <mustafapir29@gmail.com>
2020-06-05 16:42:59 +0300

    Make some ui changes

--- server.R
+++ server.R
@@ -528,7 +528,7 @@ server <- function(input, output, session) {
                         
                     )
                 ),
-                top = "50px",
+                top = "20px",
                 left = 0,
                 right = 0,
                 fixed = FALSE,
